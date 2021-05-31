90dbcf3baf11d04dbf166c76d9b8f11d21a39e78
SpoortiT <spoorti2015@gmail.com>
2021-05-31 18:41:21 -0400

    I am adding the memes screen.

---
+++ app/src/main/java/com/example/helpinghands/Adapter.java
@@ -0,0 +1,9 @@
+package com.example.helpinghands;
+
+import android.content.Context;
+
+public class Adapter {
+
+    private Context ctx;
+    private int [] ImageArray= new int [] {R. drawable. }
+}
